663798bd49bcac6fc9aae90e113560482336588c
kwasniew <kwasniewski.mateusz@gmail.com>
2019-11-24 18:19:36 +0100

    gzip compression

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
+var compression = require("compression");
 var app = express();
 
+app.use(compression());
 app.use(express.static(__dirname));
 
 var port = process.env.PORT || 8080;
