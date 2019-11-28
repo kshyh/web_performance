5d2f002fec077b2081993d44bd25dcbfede2a166
kwasniew <kwasniewski.mateusz@gmail.com>
2019-11-28 14:47:11 +0100

    Caching strategy

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 var express = require("express");
 var compression = require("compression");
 var shrinkRay = require("shrink-ray-current");
+var mime = require("mime");
 var app = express();
 
 app.use(shrinkRay());
 // app.use(compression());
-app.use(express.static("dist", {maxAge: "10s"}));
+app.use(express.static("dist", {
+    setHeaders(res, path) {
+        var fileType = mime.getType(path);
+
+        switch (fileType) {
+            case "text/html":
+                res.setHeader("Cache-Control", "no-cache");
+                break;
+            case "text/css":
+            case "text/javascript":
+            case "application/javascript":
+            case "font/font-woff":
+            case "font/font-woff2":
+            case "image/png":
+            case "image/jpeg":
+            case "image/svg+xml":
+            case "image/webp":
+                res.setHeader("Cache-Control", "max-age=" + (30 * 24 * 60 * 60));
+                break;
+        }
+    }
+}));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
