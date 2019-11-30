498dd3376e2b53ea45fdcffa8bba5d88abcf267b
kwasniew <kwasniewski.mateusz@gmail.com>
2019-11-30 11:55:00 +0100

    Paint Timing API

--- scripts/navigationTiming.js
+++ scripts/navigationTiming.js
@@ -3,9 +3,13 @@ var timing = performance.getEntriesByType("navigation")[0];
 var timeToFirstByte = timing.responseStart - timing.fetchStart;
 var domContentLoaded = timing.domContentLoadedEventEnd - timing.fetchStart;
 var loadTime = timing.loadEventEnd - timing.fetchStart;
+var timeToFirstPaint = performance.getEntriesByType('paint')[0].startTime;
+var timeToFirstContentFulPaint = performance.getEntriesByType('paint')[1].startTime;
 
 var results = {
     timeToFirstByte,
+    timeToFirstPaint,
+    timeToFirstContentFulPaint,
     domContentLoaded,
     loadTime
 
