708d15b5ebfcdf6c40ba112213f2aa6d10adbbce
kwasniew <kwasniewski.mateusz@gmail.com>
2019-12-01 17:47:48 +0100

    SW lifecycle events

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,12 @@
-console.log("SW1 script run at " + new Date());
+console.log("SW script run at " + new Date());
+
+self.addEventListener("install", function(event) {
+    console.log("SW installed at", new Date().toLocaleTimeString());
+});
+
+self.addEventListener("activate", function(event) {
+    console.log("SW activated at", new Date().toLocaleTimeString());
+});
 
 self.addEventListener("fetch", function(event) {
     console.log("Fetching", event.request);
